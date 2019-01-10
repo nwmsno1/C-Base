792ca5d82cb33bfbd3fa9d0e93ad9a93ac6c7da1
nwmsno1 <410165862@qq.com>
2019-01-10 11:07:09 +0800

    Create struct3.c

---
+++ struct/struct3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Books
+{
+	char title[50];
+	char author[50];
+	char subject[100];
+	int book_id;
+};
+
+
+//function declare
+void printBook( struct Books *book );
+int main()
+{
+	struct Books Book1;
+	struct Books Book2;
+
+	//Book1
+	strcpy( Book1.title, "C Programming");
+	strcpy( Book1.author, "Nuha Ali");
+	strcpy( Book1.subject, "Telecom Biling Tutorial");
+	Book1.book_id = 6495407;
+
+	//Book2
+	strcpy( Book2.title, "Telecom Billing");
+	strcpy( Book2.author, "Zara Ali");
+	strcpy( Book2.subject, "Telecom Billing Tutorial");
+	Book2.book_id = 6495700;
+	
+	/* 输出 Book1 信息 */
+	printBook(&Book1);
+
+   	/* 输出 Book2 信息 */
+	printBook(&Book2);
+	
+	return 0;
+}
+
+void printBook( struct Books *book )
+{
+	printf( "Book title : %s\n", book->title);
+        printf( "Book author : %s\n", book->author);
+        printf( "Book subject : %s\n", book->subject);
+        printf( "Book book_id : %d\n", book->book_id);
+}
