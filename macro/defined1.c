4eeed80d6e87ff64305535c199c9835cf755e5d9
nwmsno1 <410165862@qq.com>
2019-01-23 13:45:17 +0800

    Create defined1.c

---
+++ macro/defined1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define square(x) ((x) * (x))
+
+#define square_1(x) (x * x)
+
+int main(void)
+{
+        printf("square 5+4 is %d\n", square(5+4));
+        printf("square_1 5+4 is %d\n", square_1(5+4));
+        return 0;
+}
